2f4158d2e61f658e6f60d4aa964490b0c66df33b
santoslinares1 <slinares@mi.unc.edu.ar>
2024-09-15 21:16:47 -0300

    cambios

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,12 +1,16 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-header',
   standalone: true,
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, CommonModule],
   templateUrl: './header.component.html',
   styleUrl: './header.component.css'
 })
 export class HeaderComponent {
-
+  isMenuOpen: boolean = false;
+  toggleMenu() {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
