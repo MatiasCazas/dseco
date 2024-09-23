e2ccc455caefc7c6a5431c5ade8b731d3cc5a981
santoslinares1 <slinares@mi.unc.edu.ar>
2024-09-23 16:55:54 -0300

    Mejoras

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy, NgZone } from '@angular/core';
 import { ArticulosService } from '../services/articulos.service';
 import { CommonModule } from '@angular/common';
 
@@ -9,19 +9,22 @@ import { CommonModule } from '@angular/common';
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit, OnDestroy {
 
   constructor(
-    private articulosService: ArticulosService
+    private articulosService: ArticulosService,
+    private ngZone: NgZone
   ) {}
 
   articulos: any[] = []
 
-  images: string[] = ['assets/images/Banners-Digi-seco_1.png','assets/images/Banners-Digi-seco_2.png'];
-  currentIndex = 0;
-  translateValue = 0; 
+  images: string[] = ['assets/images/Banners-Digi-seco_1.png', 'assets/images/Banners-Digi-seco_3.png'];
+
 
   async ngOnInit(){
+    this.ngZone.runOutsideAngular(() => {
+      this.startAutoSlide();
+    });
     this.articulosService.getAllArticulos().subscribe(
       data => {
         //this.articulos.push(data.find(data[0].IdArticulo == 58))
@@ -31,27 +34,47 @@ export class HomeComponent {
       }
     )
   }
+  currentIndex = 0;
+  translateValue = 0; 
+  private intervalId: any;
+
+
+
+
+
+  ngOnDestroy() {
+    this.stopAutoSlide();
+  }
+
+  startAutoSlide() {
+    this.stopAutoSlide();
+    this.intervalId = setInterval(() => {
+      this.ngZone.run(() => {
+        this.nextSlide();
+      });
+    }, 3000);
+  }
+
+  stopAutoSlide() {
+    if (this.intervalId) {
+      clearInterval(this.intervalId);
+      this.intervalId = null;
+    }
+  }
 
   prevSlide(): void {
-    if (this.currentIndex === 0) {
-      this.currentIndex = this.images.length - 1;
-    } else {
-      this.currentIndex--;
-    }
+    this.currentIndex = (this.currentIndex > 0) ? this.currentIndex - 1 : this.images.length - 1;
     this.updateSlider();
   }
 
   nextSlide(): void {
-    if (this.currentIndex === this.images.length - 1) {
-      this.currentIndex = 0;
-    } else {
-      this.currentIndex++;
-    }
+    this.currentIndex = (this.currentIndex < this.images.length - 1) ? this.currentIndex + 1 : 0;
     this.updateSlider();
   }
 
   updateSlider(): void {
     this.translateValue = -100 * this.currentIndex;
-    console.log(this.currentIndex)
   }
+
+
 }
