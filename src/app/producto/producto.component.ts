2ea81a6780e9f011804d59284b96232e78360440
MCazas <mati.cazas99@gmail.com>
2024-11-23 17:16:45 -0300

    carrito compra

--- src/app/producto/producto.component.ts
+++ src/app/producto/producto.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-producto',
@@ -9,4 +9,14 @@ import { Component } from '@angular/core';
 })
 export class ProductoComponent {
 
+  @Input() precio: any
+  @Input() descripcion: any
+ 
+  constructor(){}
+
+  ngOnInit(){
+    console.log(this.precio)
+    console.log(this.descripcion)
+  }
+  
 }
